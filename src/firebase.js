e33376ee13454ac2bb4a4231c5dfa1a214d303e5
Axl Joven <axlmorenojoven@gmail.com>
2025-05-28 01:53:08 +0800

    Create .env

--- src/firebase.js
+++ src/firebase.js
@@ -4,18 +4,22 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Load environment variables
+require('dotenv').config();
+
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBnbuaQRTDm_U8iooFicDh_G7j2-nIa-cw",
-    authDomain: "axljoven-33680.firebaseapp.com",
-    projectId: "axljoven-33680",
-    storageBucket: "axljoven-33680.firebasestorage.app",
-    messagingSenderId: "1073281000591",
-    appId: "1:1073281000591:web:cb1ee9b95c375802833cfa",
-    measurementId: "G-98S1WDFWZG"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { app, analytics };
